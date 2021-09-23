4d5a88ffa99ad2c7d7fafec921bdbd754099d2e8
Becedas <j_becedas@hotmail.com>
2021-09-23 15:44:01 +0200

    Update 23/09/2021

--- MyHome_App/src/app/app-routing.module.ts
+++ MyHome_App/src/app/app-routing.module.ts
@@ -8,8 +8,12 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'tabs',
     pathMatch: 'full'
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./core/pages/tabs/tabs.module').then( m => m.TabsPageModule)
   },
 ];
 
